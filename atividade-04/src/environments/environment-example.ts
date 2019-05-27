9d85002dce4ccddfcaf83e8bfda4499646d3dee4
Douglasx <douglasantana007@gmail.com>
2019-05-26 22:57:51 -0300

    [ADD] Configs

---
+++ atividade-04/src/environments/environment-example.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+  production: false,
+  firebaseapp: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  }
+};
